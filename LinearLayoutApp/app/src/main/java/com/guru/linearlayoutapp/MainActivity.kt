8e2b71bfbf2aea2fff8e70bf04143acd890efdd6
hoyeon <aviationlovers@naver.com>
2024-07-01 23:16:25 +0900

    7/1 안드로이드 레이아웃

--- LinearLayoutApp/app/src/main/java/com/guru/linearlayoutapp/MainActivity.kt
+++ LinearLayoutApp/app/src/main/java/com/guru/linearlayoutapp/MainActivity.kt
@@ -1,16 +1,12 @@
-package com.guru.startapplication
+package com.guru.linearlayoutapp
 
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var button1 : Button
-        button1 = findViewById<Button>(R.id.button1)
     }
 }
