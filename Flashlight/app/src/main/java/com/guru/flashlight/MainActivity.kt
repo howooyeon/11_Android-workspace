234313104d50948b9a29e90f96053f2f2ac5cd18
hoyeon <aviationlovers@naver.com>
2024-07-13 04:37:02 +0900

    7/13 손전등 위젯 기능

--- Flashlight/app/src/main/java/com/guru/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/guru/flashlight/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.guru.flashlight
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var flashSwitch: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        flashSwitch = findViewById(R.id.flashSwitch)
+        val torch = Torch(this)
+        val intent = Intent(this, TorchService::class.java)
+
+        flashSwitch.setOnCheckedChangeListener(){ buttonView, isChecked ->
+            if(isChecked){
+//                torch.flashOn()
+                intent.action = "on"
+                startService(intent)
+            }else{
+                intent.action = "off"
+                startService(intent)
+//                torch.flashOff()
+            }
         }
     }
 }
